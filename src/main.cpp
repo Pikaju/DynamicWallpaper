4ddd568cbec64b089df5e4d5ed0a88319b4e6d92
Julien Scholz <7scholz@informatik.uni-hamburg.de>
2017-12-27 22:28:56 +0100

    Add FastNoiseSIMD libary

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,29 @@
 #include <iostream>
 
 #include "math/Vec3.h"
+#include "math/Ray.h"
 
-class Lol
-{
-public:
-    int kek;
-};
+#include <FastNoiseSIMD/FastNoiseSIMD.h>
 
 int main(int argc, char** argv)
 {
-    Lol lol;
-    lol.kek = 2;
-    std::cout << lol.kek << std::endl;
+    FastNoiseSIMD* myNoise = FastNoiseSIMD::NewFastNoiseSIMD();
+
+    // Get a set of 16 x 16 x 16 Simplex Fractal noise
+    float* noiseSet = myNoise->GetSimplexFractalSet(0, 0, 0, 16, 16, 16);
+    int index = 0;
+
+    for (int x = 0; x < 16; x++)
+    {
+        for (int y = 0; y < 16; y++)
+        {
+            for (int z = 0; z < 16; z++)
+            {
+                std::cout << noiseSet[index++] << std::endl;
+            }
+        }
+    }
+
+    FastNoiseSIMD::FreeNoiseSet(noiseSet);
     return 0;
 }
