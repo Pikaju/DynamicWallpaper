96ec01e22693c829ed84414b93b9e78654df2634
Julien Scholz <7scholz@informatik.uni-hamburg.de>
2017-12-28 15:20:42 +0100

    Fix Camera syntax

--- src/math/Camera.h
+++ src/math/Camera.h
@@ -12,8 +12,8 @@ public:
     T fov;
     T aspectRatio;
 
-    inline Camera(const Vec3<T>& position, T fov, T aspectRatio) : position(position), yaw(static_cast<T>(0)), pitch(static_cast<T>(0)), fov(fov), aspectRatio(aspectRatio)
-    inline Camera(const Vec3<T>& position, T yaw, T pitch, T fov, T aspectRatio) : position(position), yaw(yaw), pitch(pitch), fov(fov), aspectRatio(aspectRatio)
+    inline Camera(const Vec3<T>& position, T fov, T aspectRatio) : position(position), yaw(static_cast<T>(0)), pitch(static_cast<T>(0)), fov(fov), aspectRatio(aspectRatio) {  }
+    inline Camera(const Vec3<T>& position, T yaw, T pitch, T fov, T aspectRatio) : position(position), yaw(yaw), pitch(pitch), fov(fov), aspectRatio(aspectRatio) {  }
     inline ~Camera() {  }
 
     inline Ray<T> getViewRay(T x, T y) const { return Ray<T>(position, yaw + x * aspectRatio * fov / static_cast<T>(2), pitch + y * fov / static_cast<T>(2)); }
