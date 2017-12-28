c00746d04d2de45802a7af7cb8dd80b16b1f4b4e
Julien Scholz <7scholz@informatik.uni-hamburg.de>
2017-12-29 00:47:54 +0100

    Add water surface

--- src/world/heightmap/Heightmap.cpp
+++ src/world/heightmap/Heightmap.cpp
@@ -18,33 +18,48 @@ Heightmap::~Heightmap()
     }
 }
 
-#include <iostream>
-
 TraceResult Heightmap::trace(const Rayf& ray) const
 {
     TraceResult result;
-    float step = 1.0f;
-    float stepMultiplier = 1.0f;
-    float maxSlope = 2.0f;
 
-    for (float distance = 0.0f; ; distance += step * stepMultiplier) {
+    const float STEP = 1.0f;
+    const float MAX_SLOPE = 1.7f;
+    const float WATER_HEIGHT = -20.0f;
+
+    float stepMultiplier = 1.0f;
+
+    for (float distance = 0.0f; ; distance += STEP * stepMultiplier) {
         Vec3f currentPosition = ray.march(distance);
 
         if (currentPosition.x < 0.0f || currentPosition.z < 0.0f || currentPosition.x > getGridWidth() - 1 || currentPosition.z > getGridLength() - 1) {
             break;
         }
 
-        float height = getHeightAt(static_cast<unsigned int>(currentPosition.x), static_cast<unsigned int>(currentPosition.z));
+        // Water surface
+        if (currentPosition.y < WATER_HEIGHT) {
+            Rayf reflectedRay(currentPosition, ray.direction * Vec3f(1.0f, -1.0f, 1.0f));
+            reflectedRay.origin.y = WATER_HEIGHT;
+            result = trace(reflectedRay);
+            result.distance += distance;
+            result.diffuseColor = result.diffuseColor * Vec3f(0.25f) + Vec3f(0.1f, 0.2f, 0.2f);
+            return result;
+        }
 
-        if (currentPosition.y - height < maxSlope) {
+        // Check with the nearest neighbor if intersection is possible
+        float height = getHeightAt(static_cast<unsigned int>(currentPosition.x), static_cast<unsigned int>(currentPosition.z));
+        if (currentPosition.y - height < MAX_SLOPE) {
+            // Check for intersection with bilinear interpolation
             height = getHeightInterpolated(currentPosition.x, currentPosition.z);
             if (currentPosition.y <= height) {
                 result.intersects = true;
                 result.distance = distance;
+                result.diffuseColor = Vec3f(0.1f, 0.1f, 0.1f);
                 return result;
             }
         }
-        stepMultiplier = (currentPosition.y - height) + 0.125f;
+
+        // Set step multiplier based on distance to surface for performance increase
+        stepMultiplier = (currentPosition.y - std::max(height, WATER_HEIGHT)) / MAX_SLOPE + 0.125f;
     }
     result.intersects = false;
     return result;
