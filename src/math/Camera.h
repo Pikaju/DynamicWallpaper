4160a051bd4b8b8f2c8d5f38172a4f256d887827
Julien Scholz <7scholz@informatik.uni-hamburg.de>
2017-12-28 12:49:35 +0100

    Fix the camera's field of viewing being twice as big as it should be

--- src/math/Camera.h
+++ src/math/Camera.h
@@ -16,7 +16,7 @@ public:
     inline Camera(const Vec3<T>& position, T yaw, T pitch, T fov, T aspectRatio) : position(position), yaw(yaw), pitch(pitch), fov(fov), aspectRatio(aspectRatio)
     inline ~Camera() {  }
 
-    inline Ray<T> getViewRay(T x, T y) const { return Ray<T>(position, yaw + x * aspectRatio * fov, pitch + y * fov); }
+    inline Ray<T> getViewRay(T x, T y) const { return Ray<T>(position, yaw + x * aspectRatio * fov / static_cast<T>(2), pitch + y * fov / static_cast<T>(2)); }
     inline Vec3<T> getViewDirection(T x, T y) const { return getViewRay(x, y).direction; }
     inline Vec3<T> getViewDirection() const { return getViewDirection(static_cast<T>(0), static_cast<T>(0)); }
 };
