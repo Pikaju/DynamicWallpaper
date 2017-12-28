0cf2ed14914d09c36080afb76ae8886d046ce78d
Julien Scholz <7scholz@informatik.uni-hamburg.de>
2017-12-29 00:48:34 +0100

    Make example scene setup a bit better

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 
 int main(int argc, char** argv)
 {
-    RGBImage image(1280, 720);
+    RGBImage image(1920, 1080);
     float aspectRatio = static_cast<float>(image.getWidth()) / static_cast<float>(image.getHeight());
     Camera<float> camera(Vec3f(0.0f, 32.0f, 0.0f), 45.0f, -10.0f, 90.0f / aspectRatio, aspectRatio);
 
