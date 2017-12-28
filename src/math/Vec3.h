e096a4cff7ac85e12d0647e025a7d2a7a93eaa4b
Julien Scholz <7scholz@informatik.uni-hamburg.de>
2017-12-28 15:08:37 +0100

    Add extra extra functionality to Vec3 and fix bugs

--- src/math/Vec3.h
+++ src/math/Vec3.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <math.h>
+
 template<typename T>
 class Vec3
 {
@@ -15,6 +17,11 @@ public:
     inline void set(T v) { set(v, v, v); }
     inline void set(T x, T y, T z) { this->x = x; this->y = y; this->z = z; }
 
+    inline T lengthSquared() const { return x * x + y * y + z * z; }
+    inline T length() const { return static_cast<T>(sqrt(lengthSquared())); }
+    inline void normalize() { this->operator/=(length()); }
+    inline Vec3<T> normalized() const { return this->operator/(length()); }
+
     inline Vec3<T> operator+(const Vec3<T>& v) const { return Vec3(x + v.x, y + v.y, z + v.z); }
     inline Vec3<T> operator-(const Vec3<T>& v) const { return Vec3(x - v.x, y - v.y, z - v.z); }
     inline Vec3<T> operator*(const Vec3<T>& v) const { return Vec3(x * v.x, y * v.y, z * v.z); }
@@ -25,15 +32,15 @@ public:
     inline Vec3<T> operator*(T v) const { return Vec3(x * v, y * v, z * v); }
     inline Vec3<T> operator/(T v) const { return Vec3(x / v, y / v, z / v); }
 
-    inline void operator+(const Vec3<T>& v) { set(x + v.x, y + v.y, z + v.z); }
-    inline void operator-(const Vec3<T>& v) { set(x - v.x, y - v.y, z - v.z); }
-    inline void operator*(const Vec3<T>& v) { set(x * v.x, y * v.y, z * v.z); }
-    inline void operator/(const Vec3<T>& v) { set(x / v.x, y / v.y, z / v.z); }
+    inline void operator+=(const Vec3<T>& v) { set(x + v.x, y + v.y, z + v.z); }
+    inline void operator-=(const Vec3<T>& v) { set(x - v.x, y - v.y, z - v.z); }
+    inline void operator*=(const Vec3<T>& v) { set(x * v.x, y * v.y, z * v.z); }
+    inline void operator/=(const Vec3<T>& v) { set(x / v.x, y / v.y, z / v.z); }
 
-    inline void operator+(T v) { set(x + v, y + v, z + v); }
-    inline void operator-(T v) { set(x - v, y - v, z - v); }
-    inline void operator*(T v) { set(x * v, y * v, z * v); }
-    inline void operator/(T v) { set(x / v, y / v, z / v); }
+    inline void operator+=(T v) { set(x + v, y + v, z + v); }
+    inline void operator-=(T v) { set(x - v, y - v, z - v); }
+    inline void operator*=(T v) { set(x * v, y * v, z * v); }
+    inline void operator/=(T v) { set(x / v, y / v, z / v); }
 };
 
 typedef Vec3<int> Vec3i;
