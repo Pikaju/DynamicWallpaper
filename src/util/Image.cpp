5dc01fc43eafd504a3df2e302583f6970e552896
Julien Scholz <7scholz@informatik.uni-hamburg.de>
2018-01-01 15:10:32 +0100

    Add HDRImage class

--- src/util/Image.cpp
+++ src/util/Image.cpp
@@ -3,6 +3,7 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include <stb/stb_image_write.h>
 
+#define CLAMP(x, min, max) (x < min ? min : (x > max ? max : x))
 
 RGBImage::RGBImage(unsigned int width, unsigned int height) : m_width(width), m_height(height), m_pixels(width * height * 3)
 {
@@ -15,4 +16,23 @@ RGBImage::~RGBImage()
 void RGBImage::writeToJPEG(const char* file, int quality) const
 {
     stbi_write_jpg(file, m_width, m_height, 3, reinterpret_cast<const float*>(m_pixels.data()), quality);
+}
+
+HDRImage::HDRImage(unsigned int width, unsigned int height) : m_width(width), m_height(height), m_pixels(width * height * 3)
+{
+}
+
+HDRImage::~HDRImage()
+{
+}
+
+void HDRImage::toRGBImage(RGBImage& image) const
+{
+    for (unsigned int x = 0; x < getWidth(); x++) {
+        for (unsigned int y = 0; y < getHeight(); y++) {
+            image.setRed(x, y, static_cast<unsigned char>(CLAMP(getRed(x, y), 0.0f, 1.0f) * 255.0f));
+            image.setGreen(x, y, static_cast<unsigned char>(CLAMP(getGreen(x, y), 0.0f, 1.0f) * 255.0f));
+            image.setBlue(x, y, static_cast<unsigned char>(CLAMP(getBlue(x, y), 0.0f, 1.0f) * 255.0f));
+        }
+    }
 }
--- src/util/Image.h
+++ src/util/Image.h
@@ -2,6 +2,8 @@
 
 #include <vector>
 
+#include "../math/Vec3.h"
+
 class RGBImage
 {
 public:
@@ -36,4 +38,36 @@ private:
     unsigned int m_width;
     unsigned int m_height;
     std::vector<unsigned char> m_pixels;
+};
+
+class HDRImage
+{
+public:
+    HDRImage(unsigned int width, unsigned int height);
+    ~HDRImage();
+
+    void toRGBImage(RGBImage& image) const;
+
+    inline float getRed(unsigned int x, unsigned int y) const { return m_pixels[(x + y * m_width) * 3 + 0]; }
+    inline float getGreen(unsigned int x, unsigned int y) const { return m_pixels[(x + y * m_width) * 3 + 1]; }
+    inline float getBlue(unsigned int x, unsigned int y) const { return m_pixels[(x + y * m_width) * 3 + 2]; }
+
+    inline void setRed(unsigned int x, unsigned int y, float value) { m_pixels[(x + y * m_width) * 3 + 0] = value; }
+    inline void setGreen(unsigned int x, unsigned int y, float value) { m_pixels[(x + y * m_width) * 3 + 1] = value; }
+    inline void setBlue(unsigned int x, unsigned int y, float value) { m_pixels[(x + y * m_width) * 3 + 2] = value; }
+
+    inline Vec3f getRGB(unsigned int x, unsigned int y) const { return Vec3f(getRed(x, y), getGreen(x, y), getBlue(x, y)); }
+    inline void setRGB(unsigned int x, unsigned int y, const Vec3f& rgb)
+    {
+        setRed(x, y, rgb.x);
+        setGreen(x, y, rgb.y);
+        setBlue(x, y, rgb.z);
+    }
+
+    inline unsigned int getWidth() const { return m_width; }
+    inline unsigned int getHeight() const { return m_height; }
+private:
+    unsigned int m_width;
+    unsigned int m_height;
+    std::vector<float> m_pixels;
 };
